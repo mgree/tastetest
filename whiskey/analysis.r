8956efedbf62fea79fc6f7dc9d118d8f5d524387
Michael Greenberg <mgree@seas.upenn.edu>
2013-05-01 13:18:22 -0400

    fixup reporting code to make sure the directory exists

--- whiskey/analysis.r
+++ whiskey/analysis.r
@@ -5,6 +5,8 @@ library("ggplot2")
 whiskies <- read.table("whiskies.txt",header=TRUE,sep=",",quote="\"")
 tasters <- data.frame(read.table("tasters.txt",header=TRUE,sep=","))
 
+# report config
+reports = "reports"
 fresh = TRUE
 
 cleanName <- function(file) {
@@ -46,20 +48,24 @@ drawPlot <- function (p,out,width=800,height=600) {
   }
 }
 
+if (!file.exists(reports)) {
+  dir.create(reports)
+}
+
 # overall
 drawPlot(ggplot(ratings) +
          geom_boxplot(aes(Whiskey,Overall),outlier.size=0) +
          geom_text(aes(x=Whiskey,y=Overall,colour=Taster,label=Taster),
                      size=4, position=position_jitter(width=.2,height=.05)) +
          guides(colour=FALSE) + ggtitle("Overall ratings"),
-         "reports/overall.png",width=2000,height=500)
+         file.path(reports,"overall.png"),width=2000,height=500)
 # overall by gender
 drawPlot(ggplot(ratings) +
          geom_boxplot(aes(Whiskey,Overall,fill=Gender),outlier.size=0) +
          geom_text(aes(x=Whiskey,y=Overall,colour=Taster,label=Taster),
                      size=4, position=position_jitter(width=.2,height=.05)) +
          guides(colour=FALSE) + ggtitle("Overall ratings by gender"),
-         "reports/overall_gender.png",width=2000,height=500)
+         file.path(reports,"overall_gender.png"),width=2000,height=500)
 
 # mellowness vs. proof
 drawPlot(ggplot(ratings,aes(x=Proof,y=Mellowness,colour=Taster)) +
@@ -69,10 +75,10 @@ drawPlot(ggplot(ratings,aes(x=Proof,y=Mellowness,colour=Taster)) +
          geom_smooth(aes(group=1,colour="red"),na.rm=TRUE,method="loess") +
          geom_smooth(aes(group=1,colour="blue"),na.rm=TRUE,method="lm") +
          guides(colour=FALSE) + ggtitle("Mellowness vs. proof"),
-         "reports/mellowness_proof.png")
+         file.path(reports,"mellowness_proof.png"))
 drawPlot(ggplot(ratings,aes(x=reorder(Whiskey,Proof),y=Mellowness)) +
          geom_violin(aes(fill=Proof)) +
          geom_text(aes(label=Taster,colour=Taster),
                    position=position_jitter(),size=4) +
          xlab("Whiskey") + guides(colour=FALSE) + ggtitle("Mellowness vs. proof, by whiskey"),
-         "reports/mellowness_proof_violin.png",width=1500)
+         file.path(reports,"mellowness_proof_violin.png"),width=1500)
